4845b6b567f78c628feb7bae3eb04a8672ae5a81
martinm236 <t.openi36@gmail.com>
2021-12-05 21:06:46 +0100

    Formated

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import pygame
-import sys 
+import sys
 import pathlib
 import random
-
-from pygame.constants import WINDOWENTER
 
 pygame.init()
 WIDTH, HEIGHT = 1000, 500
@@ -13,13 +11,14 @@ pygame.display.set_caption("project")
 WHITE = 255, 255, 255
 print(sys.argv[0])
 print(pathlib.Path(sys.argv[0]))
-BASE  = pathlib.Path(sys.argv[0]).parent
+BASE = pathlib.Path(sys.argv[0]).parent
 FPS = 60
 FONT = pygame.font.SysFont("Arial", 30)
 P1_W = FONT.render("Hráč vyhrál.", True, (0, 0, 0))
 C1_W = FONT.render("Počítač vyhrál.", True, (0, 0, 0))
 P1 = FONT.render("HRÁČ", True, (0, 0, 0))
 C1 = FONT.render("POČÍTAČ", True, (0, 0, 0))
+
 
 class Cross(pygame.Rect):
     def __init__(self, x, y, w, h):
@@ -31,8 +30,9 @@ class Cross(pygame.Rect):
         self.w = w
         self.h = h
 
+
 class Board:
-    def __init__(self, name: str, top_left: tuple=(0, 0)):
+    def __init__(self, name: str, top_left: tuple = (0, 0)):
         self.name = name
         self.top_left = top_left
         self.won = False
@@ -42,7 +42,8 @@ class Board:
         self.rectangles = [[0] * 10 for _ in range(10)]
         for w in range(10):
             for i in range(10):
-                self.rectangles[w][i] = Cross(i * 35 + self.top_left[0], w * 35 + self.top_left[1], 30, 30)
+                self.rectangles[w][i] = Cross(
+                    i * 35 + self.top_left[0], w * 35 + self.top_left[1], 30, 30)
 
     def draw(self):
         for w in range(10):
@@ -50,9 +51,9 @@ class Board:
                 if self.rectangles[w][i].chosen:
                     pygame.draw.rect(WIN, (255, 0, 0), self.rectangles[w][i])
                 else:
-                   pygame.draw.rect(WIN, (0, 0, 0), self.rectangles[w][i])
+                    pygame.draw.rect(WIN, (0, 0, 0), self.rectangles[w][i])
         pygame.display.update()
-    
+
     def on_click(self):
         if self.number_of_pairs_chosen == 10:
             player_turn()
@@ -65,9 +66,10 @@ class Board:
                         if self.number_of_pairs_chosen == 10:
                             enemy_choice()
                     if self.rectangles[w][i].chosen:
-                        pygame.draw.rect(WIN, (255, 0, 0), self.rectangles[w][i])
+                        pygame.draw.rect(WIN, (255, 0, 0),
+                                         self.rectangles[w][i])
             pygame.display.update()
-        
+
     def get_rectangles_shot(self):
         a = 0
         for w in range(10):
@@ -75,11 +77,12 @@ class Board:
                 if self.rectangles[w][i].shot:
                     a += 1
         return a
-    
+
+
 def enemy_choice():
     for i, y in get_enemy_coordinates():
         b.rectangles[i][y].chosen = True
-            
+
 
 def player_turn():
     for w in range(10):
@@ -99,7 +102,7 @@ def player_turn():
         won = True
         WIN.blit(P1_W, (500, 440))
     pygame.display.update()
-    
+
 
 def computer_turn():
     e = 0
@@ -124,11 +127,12 @@ def computer_turn():
         WIN.blit(P1_W, (500, 440))
     pygame.display.update()
 
+
 def get_enemy_coordinates():
     enemy = [[None] * 2 for _ in range(10)]
     e = 0
     while e < 10:
-        x = random.randint(0, 9)     
+        x = random.randint(0, 9)
         y = random.randint(0, 9)
         if [x, y] not in enemy:
             enemy[e][0] = x
@@ -136,11 +140,13 @@ def get_enemy_coordinates():
             e += 1
     return enemy
 
+
 def draw_window():
     WIN.fill(WHITE)
     WIN.blit(P1, (100, 380))
     WIN.blit(C1, (600, 380))
     pygame.display.update()
+
 
 def main():
     global mouse_pos
@@ -162,16 +168,10 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
             elif event.type == pygame.MOUSEBUTTONDOWN and not won:
-                    a.on_click()
-
-        
-         
-        keys_pressed = pygame.key.get_pressed()
-        
-        
-        
+                a.on_click()
 
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
